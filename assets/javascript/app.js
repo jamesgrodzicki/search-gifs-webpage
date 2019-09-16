65f851ba525c230c2422812e8a73b524f1f3e35b
James Grodzicki <jamespgrodzicki@gmail.com>
2019-09-16 16:56:39 -0500

    made buttons and images dynamically appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,18 +8,33 @@ function displayTopics(){
         method: "GET"
     }).then(function(response){
         console.log(response);
+        results = response.data;
+        for(var i=0;i<results.length;i++){
+            var topicDiv = $("<div>");
+            var ratingTag = $("<p>");
+            $(ratingTag).text("Rating: " + results[i].rating);
+            var imgTag = $("<img>");
+            imgTag.attr("src", results[i].images.fixed_height_still.url);
+            $(topicDiv).append(imgTag, ratingTag);
+            $(".gifs").prepend(topicDiv)
+        }
     })
 }
 
 function renderButtons(){
-    $(".button-container").empty();
+    $("#button-container").empty();
     for(var i=0; i<topics.length; i++){
-        $("#button-container").append(`<button type="button" class="btn btn-light" data-topic="${topics[i]}">${topics[i]}</button>`);
+        $("#button-container").append(`<button type="button" class="btn btn-light gif-button" data-topic="${topics[i]}">${topics[i]}</button>`);
     }
 }
 
 $(document).ready(function(){
-
     renderButtons();
-    displayTopics();
+    $("#add-game").on("click", function(event){
+        event.preventDefault();
+        var newGame = $("#game-input").val().trim();
+        topics.push(newGame);
+        renderButtons();
+    });
+    $(document).on("click", ".gif-button", displayTopics);
 });
